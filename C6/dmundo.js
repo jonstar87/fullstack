d45275f87441860467788c890624b2b5ac49748c
JONA <jona@MacBook-Pro-de-JONA.local>
2021-12-22 11:23:28 -0600

    c6 destruct

---
+++ C6/dmundo.js
@@ -0,0 +1,8 @@
+let destinosDelMundo = ['Marruecos', 'Bariloche', 'Barcelona', 'China', 'Grecia'];
+
+let [primero, segundo, tercero, cuarto] = destinosDelMundo;
+
+//let [2, 3, 4, china] = destinosDelMundo;
+
+console.log(segundo);
+console.log(cuarto);
