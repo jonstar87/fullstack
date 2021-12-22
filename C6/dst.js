4c6d1c103fbef349eb75c635f3c6f5d52147c841
Mac JONADOM <jona.dominguez@outlook.com>
2021-12-22 11:44:44 -0600

    dst de air

---
+++ C6/dst.js
@@ -0,0 +1,19 @@
+let notas = [10, 4 , 5, 8 , 9 , 2, 7];
+
+let [primero, segundo] = notas;
+
+console.log(notas);
+console.log(primero);
+console.log(segundo);
+
+
+let persona = {nombre: "Jon", profesion: "QFB", edad: 34};
+
+
+let {nombre, edad} = persona;
+
+console.log(persona);
+console.log(nombre);
+console.log(edad);
+
+
